001a2135a1de802fdaa232fed0d18abc372ff603
ferleo626 <patio.fernando9@gmail.com>
2026-04-07 11:13:00 -0300

    app en funcion

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://TU-FRONTEND.onrender.com"
+   "https://hardcorelogs.vercel.app"
 ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
