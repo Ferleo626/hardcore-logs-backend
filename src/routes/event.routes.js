c349dd16da6dbffacbfcdb19ac90bab153c99fff
ferleo626 <patio.fernando9@gmail.com>
2026-04-15 13:47:02 -0300

    prueba de resumen inteligente backend

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
-import { createEvent, getEventsByWorld } from "../controllers/event.controller.js";
+import { createEvent, getEventsByWorld, generateSummary } from "../controllers/event.controller.js";
 import { authMiddleware } from "../middleware/auth.js";
 
 const router = Router();
 
-// 💎 CREAR EVENTO (SIN lógica acá)
+// 💎 CREAR EVENTO
 router.post("/", authMiddleware, createEvent);
+
+// 📊 RESUMEN DEL MUNDO
+router.get("/summary/:worldId", generateSummary);
 
 // 📜 OBTENER EVENTOS
 router.get("/:worldId", getEventsByWorld);
+
 
 export default router;
